210d4b9ad54ccbf7a5d901d6d9c92b469d8139e4
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-10-16 15:33:14 +0900

    features done - 1. useEffect error 2.deploy 3. css left

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome back o</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
