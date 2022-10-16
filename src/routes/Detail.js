f1dc98d0de88224adcb42f47348aea32d1c5518e
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2022-10-16 18:54:18 +0900

    useEffect error done - 1. deploy left

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [movie, setMovie] = useState([]);
-  const getMovie = async () => {
+  const getMovie = useCallback(async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     setMovie(json.data.movie);
     setLoading(false);
-  };
+  }, [id]);
   useEffect(() => {
     getMovie();
-  }, []);
+  }, [getMovie]);
 
   return (
     <div className="App">
